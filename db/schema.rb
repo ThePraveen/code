f885ee3d1403e2c601a202a91bcea56d07e3577f
Praveen Sah <thepraveen0207@gmail.com>
2017-10-08 20:33:55 +0530

    bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017122835) do
+ActiveRecord::Schema.define(version: 20171008120307) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",       limit: 65535
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20161017122835) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "name"
+    t.string   "status"
   end
 
 end
