9267245465447988b46475ba4347e369b6fe5d4c
Nitesh Kumar <nitesh9952@gmail.com>
2017-10-08 13:26:51 +0530

    Conflict fix for agent.rb

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,9 +1,5 @@
 class Agent < User
   has_many :tickets, class_name: "Ticket", foreign_key: "agent_id"
-<<<<<<< HEAD
-
-=======
->>>>>>> e84c732f3fa85e9758b639a632b26eeebf815482
   def allowed_tickets
     Ticket.where('agent_id=:id or agent_id is null or customer_id=:id ', id: id)
   end
@@ -11,12 +7,9 @@ class Agent < User
   def ticket(ticket_id)
     Ticket.where('(agent_id=:id or customer_id=:id  or agent_id is null )and id=:ticket_id', id: id, ticket_id: ticket_id).first
   end
-<<<<<<< HEAD
 
   def last_month_closed_tickets
   	last_month=DateTime.now.beginning_of_day.last_month
   	self.tickets.where(:updated_at => [last_month.beginning_of_month..last_month.end_of_month])
   end
-=======
->>>>>>> e84c732f3fa85e9758b639a632b26eeebf815482
 end
