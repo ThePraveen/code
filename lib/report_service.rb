d882c60f65a80ad9f9b3be6231a0a6af5fce6b00
Praveen Sah <thepraveen0207@gmail.com>
2017-10-09 09:17:15 +0530

    report

--- lib/report_service.rb
+++ lib/report_service.rb
@@ -1,7 +1,7 @@
 class ReportService
   class << self
+
     def generate_report(tickets)
-      byebug
       pdf = ReportPdf.new tickets
       file_path = "#{Rails.root.to_s}/public/downloaded_reports/reportsreport_#{Time.now.to_i}.pdf"
       pdf.render_file(file_path)
@@ -11,6 +11,23 @@ class ReportService
     def generate_and_send_report(tickets)
       file_path = generate_report(tickets)
       UserMailer.send_monthly_report(self, file_path).deliver!
+    end
+
+    def generate_and_send_report_monthly
+      Admin.all.each do |admin|
+        start_date = 1.month.ago.beginning_of_month
+        end_date = 1.month.ago.end_of_month
+        tickets = admin.tickets_closed_between(start_date, end_date)
+        generate_and_send_report(tickets)
+      end
+
+      Agent.all.each do |agent|
+        start_date = 1.month.ago.beginning_of_month
+        end_date = 1.month.ago.end_of_month
+        tickets = agent.tickets_closed_between(start_date, end_date)
+        generate_and_send_report(tickets)
+      end
+
     end
   end
 end
